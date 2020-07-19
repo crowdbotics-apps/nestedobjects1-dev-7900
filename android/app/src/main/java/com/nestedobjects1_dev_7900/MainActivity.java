ab42b5ac4b9e1ed436940e6448d42807c12077fc
Crowdbotics <team@crowdbotics.com>
2020-07-19 18:47:58 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/nestedobjects1_dev_7900/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nestedobjects1_dev_7900;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "nestedobjects1_dev_7900";
+    }
+}
